33b0173c356aff5b82e2dd02d1fb74ae187c08cf
Your Name <joe.scalera@gmail.com>
2017-07-25 07:46:04 -0700

    removed authenticate user from the comments_controller as that is applied through the application controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,4 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.new(comment_params)
